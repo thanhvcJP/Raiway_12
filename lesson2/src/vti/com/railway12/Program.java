2108393eb58651c1ac39b299a1c22444291889f1
Vu Cong Thanh <thanhvc@vnext.com.vn>
2021-06-06 10:06:54 +0700

    first commit

---
+++ lesson2/src/vti/com/railway12/Program.java
@@ -0,0 +1,11 @@
+package vti.com.railway12;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+
+	}
+
+}
